5d8ad3eac82e1eb65458b0ffcf4ed9c15253a233
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-02-05 10:38:51 +0530

    Implement transformed circular array logic in Java

--- 2026/02-February/05-Thursday/Solution.java
+++ 2026/02-February/05-Thursday/Solution.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] constructTransformedArray(int[] nums) {
         int n = nums.length;
-        int[] result = new int[n];
+        int[] result = new int[n]; 
 
         for (int i = 0; i < n; i++) {
             if (nums[i] == 0) {
