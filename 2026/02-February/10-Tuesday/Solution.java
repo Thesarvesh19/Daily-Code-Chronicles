157efed80605abd3c77ae4804b48afdad510becb
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-02-10 10:50:39 +0530

    Create Solution.java

---
+++ 2026/02-February/10-Tuesday/Solution.java
@@ -0,0 +1,34 @@
+/*
+3719. Longest Balanced Subarray I
+--------------------------------
+A subarray is balanced if the number of distinct even numbers
+equals the number of distinct odd numbers.
+*/
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int longestBalanced(int[] nums) {
+        int n = nums.length;
+        int maxLen = 0;
+
+        for (int i = 0; i < n; i++) {
+            Set<Integer> evenSet = new HashSet<>();
+            Set<Integer> oddSet = new HashSet<>();
+
+            for (int j = i; j < n; j++) {
+                if (nums[j] % 2 == 0) {
+                    evenSet.add(nums[j]);
+                } else {
+                    oddSet.add(nums[j]);
+                }
+
+                if (evenSet.size() == oddSet.size()) {
+                    maxLen = Math.max(maxLen, j - i + 1);
+                }
+            }
+        }
+        return maxLen;
+    }
+}
