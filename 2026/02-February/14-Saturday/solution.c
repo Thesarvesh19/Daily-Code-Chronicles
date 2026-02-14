b65e8e4291daae9b7e6858616a7c0525e34f23b4
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-02-14 22:45:30 +0530

    Create solution.c

---
+++ 2026/02-February/14-Saturday/solution.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+double champagneTower(int poured, int query_row, int query_glass) {
+    double dp[101][101] = {0};
+    dp[0][0] = poured;
+
+    for (int r = 0; r < query_row; r++) {
+        for (int c = 0; c <= r; c++) {
+            if (dp[r][c] > 1.0) {
+                double overflow = (dp[r][c] - 1.0) / 2.0;
+                dp[r + 1][c] += overflow;
+                dp[r + 1][c + 1] += overflow;
+                dp[r][c] = 1.0;
+            }
+        }
+    }
+
+    if (dp[query_row][query_glass] > 1.0)
+        return 1.0;
+
+    return dp[query_row][query_glass];
+}
