a87dec2386358971203dbc61e3e69579bd522550
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-02-16 15:56:38 +0530

    Create solution.java

---
+++ 2026/02-February/16-Monday/solution.java
@@ -0,0 +1,28 @@
+/**
+ * Problem: Reverse Bits
+ *
+ * Reverse bits of a given 32-bit unsigned integer.
+ *
+ * Approach:
+ * Iterate through all 32 bits:
+ *  - Shift result left
+ *  - Add last bit of n using (n & 1)
+ *  - Shift n right
+ *
+ * Time Complexity: O(1)  (32 iterations)
+ * Space Complexity: O(1)
+ */
+public class Solution {
+
+    public int reverseBits(int n) {
+        int result = 0;
+
+        for (int i = 0; i < 32; i++) {
+            result <<= 1;
+            result |= (n & 1);
+            n >>= 1;
+        }
+
+        return result;
+    }
+}
