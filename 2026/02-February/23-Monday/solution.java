b24b27e7958ecb4587c8c736a797ee404f693e7f
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-02-23 22:13:55 +0530

    Create solution.java

---
+++ 2026/02-February/23-Monday/solution.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean hasAllCodes(String s, int k) {
+        int total = 1 << k;  // 2^k
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i <= s.length() - k; i++) {
+            set.add(s.substring(i, i + k));
+            if (set.size() == total) {
+                return true;
+            }
+        }
+
+        return set.size() == total;
+    }
+}
