8903792c4f24231eae197b90470d19c1f89725c9
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-02-25 21:10:21 +0530

    Create solution.java

---
+++ 2026/02-February/25-Wednesday/solution.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int[] sortByBits(int[] arr) {
+        Integer[] temp = new Integer[arr.length];
+
+        // Copy elements to Integer array
+        for (int i = 0; i < arr.length; i++) {
+            temp[i] = arr[i];
+        }
+
+        // Sort based on:
+        // 1. Number of set bits
+        // 2. Numeric value if bit counts are equal
+        Arrays.sort(temp, (a, b) -> {
+            int bitCompare = Integer.bitCount(a) - Integer.bitCount(b);
+            if (bitCompare == 0) {
+                return a - b;
+            }
+            return bitCompare;
+        });
+
+        // Copy back to original array
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = temp[i];
+        }
+
+        return arr;
+    }
+}
