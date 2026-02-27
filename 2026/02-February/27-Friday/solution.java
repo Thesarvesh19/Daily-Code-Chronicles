2ea3f64b4783e6b498eb5cb468c606392ea074d2
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-02-27 20:59:35 +0530

    Update solution.java

--- 2026/02-February/27-Friday/solution.java
+++ 2026/02-February/27-Friday/solution.java
@@ -1,1 +1,59 @@
+import java.util.*;
 
+class Solution {
+    public int minOperations(String s, int k) {
+        int n = s.length();
+
+        TreeSet<Integer>[] ts = new TreeSet[2];
+        Arrays.setAll(ts, i -> new TreeSet<>());
+
+        // Initialize possible counts (0 to n)
+        for (int i = 0; i <= n; i++) {
+            ts[i % 2].add(i);
+        }
+
+        // Count initial zeros
+        int cnt0 = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '0') {
+                cnt0++;
+            }
+        }
+
+        // Remove starting state
+        ts[cnt0 % 2].remove(cnt0);
+
+        Deque<Integer> q = new ArrayDeque<>();
+        q.offer(cnt0);
+
+        int ans = 0;
+
+        // BFS over possible zero counts
+        while (!q.isEmpty()) {
+            for (int size = q.size(); size > 0; --size) {
+                int cur = q.poll();
+
+                // If no zeros left
+                if (cur == 0) {
+                    return ans;
+                }
+
+                // Compute possible new zero range
+                int l = cur + k - 2 * Math.min(cur, k);
+                int r = cur + k - 2 * Math.max(k - n + cur, 0);
+
+                TreeSet<Integer> t = ts[l % 2];
+
+                Integer next = t.ceiling(l);
+                while (next != null && next <= r) {
+                    q.offer(next);
+                    t.remove(next);
+                    next = t.ceiling(l);
+                }
+            }
+            ans++;
+        }
+
+        return -1;
+    }
+}
