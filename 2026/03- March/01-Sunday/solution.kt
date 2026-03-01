45bc5702f1cf02ec8246d668e9ac169c0b2ef370
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-03-01 23:15:03 +0530

    Create solution.kt

---
+++ 2026/03- March/01-Sunday/solution.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun minPartitions(n: String): Int {
+        var maxDigit = 0
+        
+        for (ch in n) {
+            maxDigit = maxOf(maxDigit, ch - '0')
+        }
+        
+        return maxDigit
+    }
+}
