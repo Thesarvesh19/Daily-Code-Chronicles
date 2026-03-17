20b1d179431052e8fe6242f7e71e8b1516659eb0
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-03-17 12:24:02 +0530

    Create solution.py

---
+++ 2026/03- March/17-Tuesday/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def largestSubmatrix(self, matrix):
+        m, n = len(matrix), len(matrix[0])
+        
+        # Build heights
+        for i in range(1, m):
+            for j in range(n):
+                if matrix[i][j] != 0:
+                    matrix[i][j] += matrix[i - 1][j]
+        
+        max_area = 0
+        
+        for row in matrix:
+            row.sort(reverse=True)
+            for j in range(n):
+                max_area = max(max_area, row[j] * (j + 1))
+        
+        return max_area
