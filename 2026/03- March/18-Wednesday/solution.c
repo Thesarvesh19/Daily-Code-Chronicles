4ca9161a682d1b547970d6787111e1f859e12239
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-03-18 22:46:39 +0530

    Create solution.c

---
+++ 2026/03- March/18-Wednesday/solution.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int countSubmatrices(int** grid, int gridSize, int* gridColSize, int k) {
+    int m = gridSize;
+    int n = gridColSize[0];
+    
+    int* colSum = (int*)calloc(n, sizeof(int));
+    int count = 0;
+
+    for (int i = 0; i < m; i++) {
+        int rowPrefix = 0;
+
+        for (int j = 0; j < n; j++) {
+            // Update column sum
+            colSum[j] += grid[i][j];
+
+            // Build prefix sum for (0,0) → (i,j)
+            rowPrefix += colSum[j];
+
+            if (rowPrefix <= k) {
+                count++;
+            }
+        }
+    }
+
+    free(colSum);
+    return count;
+}
