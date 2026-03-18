5bf4379657e4ae75b277980116b48558bfb66ee8
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-03-18 22:45:56 +0530

    Create solution.py

---
+++ 2026/03- March/18-Wednesday/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countSubmatrices(self, grid, k):
+        m, n = len(grid), len(grid[0])
+        
+        col_sum = [0] * n
+        count = 0
+        
+        for i in range(m):
+            row_prefix = 0
+            for j in range(n):
+                # accumulate column sum
+                col_sum[j] += grid[i][j]
+                
+                # prefix sum from (0,0) to (i,j)
+                row_prefix += col_sum[j]
+                
+                if row_prefix <= k:
+                    count += 1
+        
+        return count
