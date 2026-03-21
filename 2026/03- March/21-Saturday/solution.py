91432d063c00e40f96b28be56bd0cdd583152c43
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-03-21 20:16:53 +0530

    Add reverseSubmatrix method to Solution class
    Implement method to reverse a submatrix in place.

---
+++ 2026/03- March/21-Saturday/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseSubmatrix(self, grid, x, y, k):
+        # Iterate over half the rows of the submatrix
+        for i in range(k // 2):
+            for j in range(y, y + k):
+                # Swap vertically
+                grid[x + i][j], grid[x + k - 1 - i][j] = (
+                    grid[x + k - 1 - i][j],
+                    grid[x + i][j],
+                )
+        
+        return grid
