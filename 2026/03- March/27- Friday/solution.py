3cd8bf973306ca3555253a8d1c46f2c0ff708307
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-03-27 18:04:01 +0530

    Update and rename 2026/03- March/27-Friday to 2026/03- March/27- Friday/solution.py

---
+++ 2026/03- March/27- Friday/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def areSimilar(self, mat, k):
+        m = len(mat)
+        n = len(mat[0])
+        
+        shift = k % n
+        
+        if shift == 0:
+            return True
+        
+        for i in range(m):
+            row = mat[i]
+            
+            if i % 2 == 0:
+                shifted = row[shift:] + row[:shift]
+            else:
+                shifted = row[-shift:] + row[:-shift]
+            
+            if shifted != row:
+                return False
+        
+        return True
