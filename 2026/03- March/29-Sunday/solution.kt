732d093ebeb7c8cc08f417be3d9fb4e6e165b43d
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-03-29 18:20:03 +0530

    Create solution.kt

---
+++ 2026/03- March/29-Sunday/solution.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun canBeEqual(s1: String, s2: String): Boolean {
+        val s1Even = charArrayOf(s1[0], s1[2]).sortedArray()
+        val s1Odd  = charArrayOf(s1[1], s1[3]).sortedArray()
+        
+        val s2Even = charArrayOf(s2[0], s2[2]).sortedArray()
+        val s2Odd  = charArrayOf(s2[1], s2[3]).sortedArray()
+        
+        return s1Even.contentEquals(s2Even) && s1Odd.contentEquals(s2Odd)
+    }
+}
