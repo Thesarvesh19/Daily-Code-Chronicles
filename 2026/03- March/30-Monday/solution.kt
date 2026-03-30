0794c341a22128bf934414f2701810b9d1852fa0
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-03-30 09:07:01 +0530

    Add Solution class with checkStrings method

---
+++ 2026/03- March/30-Monday/solution.kt
@@ -0,0 +1,18 @@
+class Solution {
+    fun checkStrings(s1: String, s2: String): Boolean {
+        val even = IntArray(26)
+        val odd = IntArray(26)
+
+        for (i in s1.indices) {
+            if (i % 2 == 0) {
+                even[s1[i] - 'a']++
+                even[s2[i] - 'a']--
+            } else {
+                odd[s1[i] - 'a']++
+                odd[s2[i] - 'a']--
+            }
+        }
+
+        return even.all { it == 0 } && odd.all { it == 0 }
+    }
+}
