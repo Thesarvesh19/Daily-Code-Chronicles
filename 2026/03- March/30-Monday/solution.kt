62266073059888612819a3c1951dd201e4c07686
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-03-30 09:08:10 +0530

    Add Kotlin solution for Check if Strings Can Be Made Equal With Operations II with optimized O(n) approach

--- 2026/03- March/30-Monday/solution.kt
+++ 2026/03- March/30-Monday/solution.kt
@@ -2,7 +2,7 @@ class Solution {
     fun checkStrings(s1: String, s2: String): Boolean {
         val even = IntArray(26)
         val odd = IntArray(26)
-
+ 
         for (i in s1.indices) {
             if (i % 2 == 0) {
                 even[s1[i] - 'a']++
