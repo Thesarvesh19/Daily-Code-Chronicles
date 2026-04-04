049a1c1c282f6e1b728b415db4bb405b2b9a2d65
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-04-04 12:53:40 +0530

    Create solution.py

---
+++ 2026/04-April/04-Saturday/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def decodeCiphertext(self, encodedText, rows):
+        if rows == 1:
+            return encodedText
+        
+        n = len(encodedText)
+        cols = n // rows
+        
+        matrix = []
+        index = 0
+        for i in range(rows):
+            matrix.append(list(encodedText[index:index + cols]))
+            index += cols
+        
+        res = []
+        for start in range(cols):
+            i, j = 0, start
+            while i < rows and j < cols:
+                res.append(matrix[i][j])
+                i += 1
+                j += 1
+        
+        return "".join(res).rstrip()
