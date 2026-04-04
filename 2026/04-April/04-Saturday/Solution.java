b26c961ce11ecfcccc46fe4c3bbb93a323746bdc
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-04-04 12:56:20 +0530

    Create Solution.java

---
+++ 2026/04-April/04-Saturday/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String decodeCiphertext(String encodedText, int rows) {
+        if (rows == 1) return encodedText;
+
+        int n = encodedText.length();
+        int cols = n / rows;
+
+        char[][] matrix = new char[rows][cols];
+        int idx = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = encodedText.charAt(idx++);
+            }
+        }
+
+        StringBuilder res = new StringBuilder();
+
+        for (int start = 0; start < cols; start++) {
+            int i = 0, j = start;
+            while (i < rows && j < cols) {
+                res.append(matrix[i][j]);
+                i++;
+                j++;
+            }
+        }
+
+        return res.toString().replaceAll("\\s+$", "");
+    }
+}
