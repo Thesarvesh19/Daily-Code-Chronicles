0185486107e34916d7be618041af90d0e55bb05a
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-04-05 10:56:56 +0530

    Add C solution for Robot Return to Origin with basic coordinate logic

--- 2026/04-April/05-Sunday/Solution.c
+++ 2026/04-April/05-Sunday/Solution.c
@@ -3,7 +3,7 @@
 bool judgeCircle(char * moves){
     int x = 0, y = 0;
     
-    for (int i = 0; moves[i] != '\0'; i++) {
+    for (int i = 0; moves[i] != '\0'; i++) { 
         if (moves[i] == 'U') y++;
         else if (moves[i] == 'D') y--;
         else if (moves[i] == 'R') x++;
