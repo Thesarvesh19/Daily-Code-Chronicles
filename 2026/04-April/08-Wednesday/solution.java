290601c4b6a59c442c3d1887be1f7f3c61f03883
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-04-08 09:27:28 +0530

    Add Java solution using direct simulation for XOR after queries

--- 2026/04-April/08-Wednesday/solution.java
+++ 2026/04-April/08-Wednesday/solution.java
@@ -5,7 +5,7 @@ class Solution {
         for (int[] q : queries) {
             int l = q[0];
             int r = q[1];
-            int k = q[2];
+            int k = q[2]; 
             int v = q[3];
 
             for (int i = l; i <= r; i += k) {
