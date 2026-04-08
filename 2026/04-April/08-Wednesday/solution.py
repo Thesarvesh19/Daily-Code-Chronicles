453a3923eeaf0a4122311e3ff617c8b228944434
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-04-08 09:26:06 +0530

    Create solution.py

---
+++ 2026/04-April/08-Wednesday/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def xorAfterQueries(self, nums, queries):
+        MOD = 10**9 + 7
+
+        for l, r, k, v in queries:
+            i = l
+            while i <= r:
+                nums[i] = (nums[i] * v) % MOD
+                i += k
+
+        result = 0
+        for num in nums:
+            result ^= num
+
+        return result
