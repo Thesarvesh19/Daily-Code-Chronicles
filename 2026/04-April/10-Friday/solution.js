24141bdaca8cb83dc094ff8a70355e947c6cd45f
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-04-10 09:31:32 +0530

    add javascript solution for minimum distance between three equal elements

--- 2026/04-April/10-Friday/solution.js
+++ 2026/04-April/10-Friday/solution.js
@@ -1,6 +1,6 @@
 var minimumDistance = function(nums) {
     let map = new Map();
-
+ 
     for (let i = 0; i < nums.length; i++) {
         if (!map.has(nums[i])) map.set(nums[i], []);
         map.get(nums[i]).push(i);
