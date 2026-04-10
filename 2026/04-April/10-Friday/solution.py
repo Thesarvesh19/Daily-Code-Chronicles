c5d1d7a378f6fbf7750e65a215fa4c09816ef6a9
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-04-10 09:32:53 +0530

    add python solution for minimum distance between three equal elements

--- 2026/04-April/10-Friday/solution.py
+++ 2026/04-April/10-Friday/solution.py
@@ -12,5 +12,5 @@ class Solution:
         for indices in pos.values():
             for i in range(len(indices) - 2):
                 ans = min(ans, 2 * (indices[i+2] - indices[i]))
-
+ 
         return ans if ans != float('inf') else -1
