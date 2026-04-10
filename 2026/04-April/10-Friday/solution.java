ffdd7c2078f557a96561ac14c24c34c494dc5903
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-04-10 09:29:07 +0530

    Create solution.java

---
+++ 2026/04-April/10-Friday/solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int minimumDistance(int[] nums) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+        }
+
+        int ans = Integer.MAX_VALUE;
+
+        for (List<Integer> list : map.values()) {
+            for (int i = 0; i <= list.size() - 3; i++) {
+                ans = Math.min(ans, 2 * (list.get(i + 2) - list.get(i)));
+            }
+        }
+
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+}
