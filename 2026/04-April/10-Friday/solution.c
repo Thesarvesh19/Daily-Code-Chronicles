7aeb61ed8b881e43ca4be1f63bc53a166d0f3161
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-04-10 09:28:14 +0530

    Create solution.c

---
+++ 2026/04-April/10-Friday/solution.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXN 101
+
+int minimumDistance(int* nums, int numsSize) {
+    int* pos[MAXN];
+    int count[MAXN] = {0};
+
+    // allocate memory
+    for (int i = 0; i < MAXN; i++) {
+        pos[i] = (int*)malloc(sizeof(int) * numsSize);
+    }
+
+    // store indices
+    for (int i = 0; i < numsSize; i++) {
+        int val = nums[i];
+        pos[val][count[val]++] = i;
+    }
+
+    int ans = 1e9;
+
+    for (int v = 0; v < MAXN; v++) {
+        if (count[v] >= 3) {
+            for (int i = 0; i + 2 < count[v]; i++) {
+                int dist = 2 * (pos[v][i+2] - pos[v][i]);
+                if (dist < ans) ans = dist;
+            }
+        }
+    }
+
+    // free memory
+    for (int i = 0; i < MAXN; i++) {
+        free(pos[i]);
+    }
+
+    return (ans == 1e9) ? -1 : ans;
+}
