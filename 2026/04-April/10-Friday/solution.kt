d7716abd88d8c6754fa2fed5413a451e2150b382
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-04-10 09:30:13 +0530

    Create solution.kt

---
+++ 2026/04-April/10-Friday/solution.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun minimumDistance(nums: IntArray): Int {
+        val map = mutableMapOf<Int, MutableList<Int>>()
+
+        for (i in nums.indices) {
+            map.getOrPut(nums[i]) { mutableListOf() }.add(i)
+        }
+
+        var ans = Int.MAX_VALUE
+
+        for (list in map.values) {
+            for (i in 0 until list.size - 2) {
+                ans = minOf(ans, 2 * (list[i + 2] - list[i]))
+            }
+        }
+
+        return if (ans == Int.MAX_VALUE) -1 else ans
+    }
+}
