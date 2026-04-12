f553da71e47abe8fa81a598875709a84dcb49876
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-04-12 12:11:56 +0530

    Update solution.py

--- 2026/04-April/12-Sunday/solution.py
+++ 2026/04-April/12-Sunday/solution.py
@@ -1,28 +1,21 @@
-class Solution {
-  public int minimumDistance(String word) {
-    Integer[][][] mem = new Integer[27][27][word.length()];
-    return minimumDistance(word, 26, 26, 0, mem);
-  }
-  private int minimumDistance(final String word, int i, int j, int k, Integer[][][] mem) {
-    if (k == word.length())
-      return 0;
-    if (mem[i][j][k] != null)
-      return mem[i][j][k];
-    final int c = word.charAt(k) - 'A';
-    final int moveLeft = dist(i, c) + minimumDistance(word, c, j, k + 1, mem);
-    final int moveRight = dist(j, c) + minimumDistance(word, i, c, k + 1, mem);
-    return mem[i][j][k] = Math.min(moveLeft, moveRight);
-  }
+import functools
 
-  private int dist(int a, int b) {
-    if (a == 26) // the first hovering state
-      return 0;
-    final int x1 = a / 6;
-    final int y1 = a % 6;
-    final int x2 = b / 6;
-    final int y2 = b % 6;
-    return Math.abs(x1 - x2) + Math.abs(y1 - y2);
-  }
-}
+class Solution:
+    def minimumDistance(self, word: str) -> int:
+        def dist(a: int, b: int) -> int:
+            if a == 26:
+                return 0
+            x1, y1 = a // 6, a % 6
+            x2, y2 = b // 6, b % 6
+            return abs(x1 - x2) + abs(y1 - y2)
 
+        @functools.lru_cache(None)
+        def dp(i: int, j: int, k: int) -> int:
+            if k == len(word):
+                return 0
+            nxt = ord(word[k]) - ord('A')
+            moveLeft = dist(i, nxt) + dp(nxt, j, k + 1)
+            moveRight = dist(j, nxt) + dp(i, nxt, k + 1)
+            return min(moveLeft, moveRight)
 
+        return dp(26, 26, 0)
