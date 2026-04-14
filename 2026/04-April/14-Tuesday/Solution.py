8f3766bb38902e8e1411e3a5a9b706cf54d9f6a5
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-04-14 11:48:47 +0530

    Create Solution.py

---
+++ 2026/04-April/14-Tuesday/Solution.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minimumTotalDistance(self, robot, factory):
+        robot.sort()
+        factory.sort()
+        
+        slots = []
+        for pos, limit in factory:
+            slots.extend([pos] * limit)
+        
+        m, n = len(robot), len(slots)
+        INF = float('inf')
+        
+        dp = [[INF] * (n + 1) for _ in range(m + 1)]
+        
+        for j in range(n + 1):
+            dp[0][j] = 0
+        
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                dp[i][j] = dp[i][j-1]
+                dp[i][j] = min(
+                    dp[i][j],
+                    dp[i-1][j-1] + abs(robot[i-1] - slots[j-1])
+                )
+        
+        return dp[m][n]
