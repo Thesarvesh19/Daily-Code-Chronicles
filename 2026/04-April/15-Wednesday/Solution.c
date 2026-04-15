d6da60ef2bf1a207d81ab160241fcf97d06bc5ef
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-04-15 12:33:30 +0530

    Create Solution.c

---
+++ 2026/04-April/15-Wednesday/Solution.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include <stdlib.h>
+
+int closestTarget(char** words, int wordsSize, char* target, int startIndex) {
+    int ans = INT_MAX;
+
+    for (int i = 0; i < wordsSize; i++) {
+        if (strcmp(words[i], target) == 0) {
+            int d = abs(i - startIndex);
+            int minDist = d < (wordsSize - d) ? d : (wordsSize - d);
+            if (minDist < ans) ans = minDist;
+        }
+    }
+
+    return ans == INT_MAX ? -1 : ans;
+}
