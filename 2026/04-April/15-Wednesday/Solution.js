0e31655d696e1ac18a70b516bc5db6f2aec2dd85
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-04-15 12:34:01 +0530

    Create Solution.js

---
+++ 2026/04-April/15-Wednesday/Solution.js
@@ -0,0 +1,13 @@
+var closestTarget = function(words, target, startIndex) {
+    let n = words.length;
+    let ans = Infinity;
+
+    for (let i = 0; i < n; i++) {
+        if (words[i] === target) {
+            let d = Math.abs(i - startIndex);
+            ans = Math.min(ans, Math.min(d, n - d));
+        }
+    }
+
+    return ans === Infinity ? -1 : ans;
+};
