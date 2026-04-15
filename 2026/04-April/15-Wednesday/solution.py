90114d6020d1e791773e9e08b4ed0cd60234617d
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-04-15 12:32:09 +0530

    Create solution.py

---
+++ 2026/04-April/15-Wednesday/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def closestTarget(self, words, target, startIndex):
+        n = len(words)
+        ans = float('inf')
+        
+        for i in range(n):
+            if words[i] == target:
+                d = abs(i - startIndex)
+                ans = min(ans, min(d, n - d))
+        
+        return -1 if ans == float('inf') else ans
