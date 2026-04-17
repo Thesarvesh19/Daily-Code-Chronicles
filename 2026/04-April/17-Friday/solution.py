2093bde37bad13e2f94c7813d485021628d5c61c
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-04-17 11:30:30 +0530

    Create solution.py

---
+++ 2026/04-April/17-Friday/solution.py
@@ -0,0 +1,22 @@
+# Minimum Absolute Distance Between Mirror Pairs
+
+class Solution:
+    def minMirrorPairDistance(self, nums):
+        def reverse(x):
+            rev = 0
+            while x:
+                rev = rev * 10 + x % 10
+                x //= 10
+            return rev
+        
+        last_seen = {}
+        ans = float('inf')
+        
+        for j, num in enumerate(nums):
+            if num in last_seen:
+                ans = min(ans, j - last_seen[num])
+            
+            rev = reverse(num)
+            last_seen[rev] = j
+        
+        return ans if ans != float('inf') else -1
