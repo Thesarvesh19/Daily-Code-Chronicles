9685e1aeb8a09e9d84b741b1c88567819624cd00
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-04-17 11:35:49 +0530

    implement mirror pair distance using hashmap in javascript

--- 2026/04-April/17-Friday/solution.js
+++ 2026/04-April/17-Friday/solution.js
@@ -5,7 +5,7 @@ var minMirrorPairDistance = function(nums) {
     let ans = Infinity;
 
     const reverse = (x) => {
-        let rev = 0;
+        let rev = 0; 
         while (x > 0) {
             rev = rev * 10 + (x % 10);
             x = Math.floor(x / 10);
