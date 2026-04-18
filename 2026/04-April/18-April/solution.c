f38f8c17b625e1f77838d5cc364e38f097a803bd
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-04-18 13:01:46 +0530

    add c implementation using digit reversal

--- 2026/04-April/18-April/solution.c
+++ 2026/04-April/18-April/solution.c
@@ -6,7 +6,7 @@ int mirrorDistance(int n) {
     int temp = n, rev = 0;
     while (temp > 0) {
         rev = rev * 10 + temp % 10;
-        temp /= 10;
+        temp /= 10; 
     }
     return abs(n - rev);
 }
