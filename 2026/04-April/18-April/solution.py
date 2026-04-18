e63746d7d96ed01c77bb86df088dc5b69eef7cdc
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-04-18 12:58:33 +0530

    Create solution.py

---
+++ 2026/04-April/18-April/solution.py
@@ -0,0 +1,12 @@
+// 3783. Mirror Distance of an Integer
+
+class Solution {
+    public int mirrorDistance(int n) {
+        int temp = n, rev = 0;
+        while (temp > 0) {
+            rev = rev * 10 + temp % 10;
+            temp /= 10;
+        }
+        return Math.abs(n - rev);
+    }
+}
