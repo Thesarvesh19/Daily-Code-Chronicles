72c85e851184cc48489368f4f80da22225c15d05
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-04-20 22:40:06 +0530

    Create solution.js

---
+++ 2026/04-April/20-April/solution.js
@@ -0,0 +1,16 @@
+// 2078. Two Furthest Houses With Different Colors
+var maxDistance = function(colors) {
+    let n = colors.length;
+    let ans = 0;
+
+    for (let i = 0; i < n; i++) {
+        if (colors[i] !== colors[0]) {
+            ans = Math.max(ans, i);
+        }
+        if (colors[i] !== colors[n - 1]) {
+            ans = Math.max(ans, n - 1 - i);
+        }
+    }
+
+    return ans;
+};
