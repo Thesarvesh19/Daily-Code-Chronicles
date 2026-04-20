7714e6050898784d0cbd60a3e7720d9fe8a6d084
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-04-20 22:50:51 +0530

    Create JavaScript solution using iteration and Math.max to determine furthest houses with different colors

--- 2026/04-April/20-April/solution.js
+++ 2026/04-April/20-April/solution.js
@@ -6,8 +6,8 @@ var maxDistance = function(colors) {
     for (let i = 0; i < n; i++) {
         if (colors[i] !== colors[0]) {
             ans = Math.max(ans, i);
-        }
-        if (colors[i] !== colors[n - 1]) {
+        } 
+        if (colors[i] !== colors[n - 1]) {       
             ans = Math.max(ans, n - 1 - i);
         }
     }
