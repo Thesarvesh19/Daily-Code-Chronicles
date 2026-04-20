861e6b851a05f6596ecd6e7718406a8f036faf4b
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-04-20 22:40:21 +0530

    Create solution.kt

---
+++ 2026/04-April/20-April/solution.kt
@@ -0,0 +1,18 @@
+// 2078. Two Furthest Houses With Different Colors
+class Solution {
+    fun maxDistance(colors: IntArray): Int {
+        val n = colors.size
+        var ans = 0
+
+        for (i in 0 until n) {
+            if (colors[i] != colors[0]) {
+                ans = maxOf(ans, i)
+            }
+            if (colors[i] != colors[n - 1]) {
+                ans = maxOf(ans, n - 1 - i)
+            }
+        }
+
+        return ans
+    }
+}
