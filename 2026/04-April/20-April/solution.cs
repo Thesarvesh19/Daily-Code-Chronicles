e6f9d482fa35ec961c64608cba2a82c7be20e049
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-04-20 22:40:47 +0530

    Create solution.cs

---
+++ 2026/04-April/20-April/solution.cs
@@ -0,0 +1,18 @@
+// 2078. Two Furthest Houses With Different Colors
+public class Solution {
+    public int MaxDistance(int[] colors) {
+        int n = colors.Length;
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (colors[i] != colors[0]) {
+                ans = Math.Max(ans, i);
+            }
+            if (colors[i] != colors[n - 1]) {
+                ans = Math.Max(ans, n - 1 - i);
+            }
+        }
+
+        return ans;
+    }
+}
