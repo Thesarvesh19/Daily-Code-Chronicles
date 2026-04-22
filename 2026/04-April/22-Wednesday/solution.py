5226fb714eb71e698c09d5e794837c566f70cfc8
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-04-22 12:41:09 +0530

    Create solution.py

---
+++ 2026/04-April/22-Wednesday/solution.py
@@ -0,0 +1,22 @@
+# Words Within Two Edits of Dictionary
+
+class Solution:
+    def twoEditWords(self, queries, dictionary):
+        
+        answer = []
+        
+        for word in queries:
+            for target in dictionary:
+                mismatch = 0
+                
+                for a, b in zip(word, target):
+                    if a != b:
+                        mismatch += 1
+                        if mismatch > 2:
+                            break
+                
+                if mismatch <= 2:
+                    answer.append(word)
+                    break
+        
+        return answer
