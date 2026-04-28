5894b753a5a4d201ed5f3035f7c252d887a32f5d
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-04-28 22:21:37 +0530

    Create solution.py

---
+++ 2026/04-April/28-Tuesday/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minOperations(self, grid, x):
+        nums = [num for row in grid for num in row]
+        nums.sort()
+
+        base = nums[0] % x
+        for num in nums:
+            if num % x != base:
+                return -1
+
+        median = nums[len(nums) // 2]
+        return sum(abs(num - median) // x for num in nums)
