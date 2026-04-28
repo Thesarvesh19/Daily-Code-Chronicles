81cd24298a5d39fe69a2b9a2fca14003478e6f9c
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-04-28 22:22:59 +0530

    Create solution.js

---
+++ 2026/04-April/28-Tuesday/solution.js
@@ -0,0 +1,23 @@
+var minOperations = function(grid, x) {
+    let nums = [];
+
+    for (let row of grid) {
+        nums.push(...row);
+    }
+
+    nums.sort((a, b) => a - b);
+
+    let base = nums[0] % x;
+    for (let num of nums) {
+        if (num % x !== base) return -1;
+    }
+
+    let median = nums[Math.floor(nums.length / 2)];
+    let ops = 0;
+
+    for (let num of nums) {
+        ops += Math.floor(Math.abs(num - median) / x);
+    }
+
+    return ops;
+};
