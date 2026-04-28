1a9fe46817aa989367026679b2032b94270ed5f9
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-04-28 22:23:17 +0530

    Create solution.go

---
+++ 2026/04-April/28-Tuesday/solution.go
@@ -0,0 +1,31 @@
+import "sort"
+
+func minOperations(grid [][]int, x int) int {
+    nums := []int{}
+
+    for _, row := range grid {
+        nums = append(nums, row...)
+    }
+
+    sort.Ints(nums)
+
+    base := nums[0] % x
+    for _, num := range nums {
+        if num%x != base {
+            return -1
+        }
+    }
+
+    median := nums[len(nums)/2]
+    ops := 0
+
+    for _, num := range nums {
+        if num > median {
+            ops += (num - median) / x
+        } else {
+            ops += (median - num) / x
+        }
+    }
+
+    return ops
+}
