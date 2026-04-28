5a87c11a07a81ba812df05c37075954c9b14ba9f
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-04-28 22:23:36 +0530

    Create solution.kt

---
+++ 2026/04-April/28-Tuesday/solution.kt
@@ -0,0 +1,27 @@
+class Solution {
+    fun minOperations(grid: Array<IntArray>, x: Int): Int {
+        val nums = mutableListOf<Int>()
+
+        for (row in grid) {
+            for (num in row) {
+                nums.add(num)
+            }
+        }
+
+        nums.sort()
+
+        val base = nums[0] % x
+        for (num in nums) {
+            if (num % x != base) return -1
+        }
+
+        val median = nums[nums.size / 2]
+        var ops = 0
+
+        for (num in nums) {
+            ops += Math.abs(num - median) / x
+        }
+
+        return ops
+    }
+}
