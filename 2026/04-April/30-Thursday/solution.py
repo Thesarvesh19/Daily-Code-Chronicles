b652cd9dea1fd98322c4b60018e8ef616c412bed
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-04-30 22:19:21 +0530

    Create solution.py

---
+++ 2026/04-April/30-Thursday/solution.py
@@ -0,0 +1,36 @@
+class Solution:
+    def maxPathScore(self, grid, k):
+        m, n = len(grid), len(grid[0])
+        k = min(k, m + n)
+
+        dp = [[-1] * (k + 1) for _ in range(n)]
+        next_dp = [[-1] * (k + 1) for _ in range(n)]
+
+        dp[0][0] = 0
+
+        for i in range(m):
+            for j in range(n):
+                next_dp[j] = [-1] * (k + 1)
+
+                if i == 0 and j == 0:
+                    next_dp[0][0] = 0
+                    continue
+
+                val = grid[i][j]
+                cost = 1 if val > 0 else 0
+
+                for c in range(cost, k + 1):
+                    prev_c = c - cost
+                    mx = -1
+
+                    if i > 0 and dp[j][prev_c] != -1:
+                        mx = dp[j][prev_c]
+                    if j > 0 and next_dp[j - 1][prev_c] != -1:
+                        mx = max(mx, next_dp[j - 1][prev_c])
+
+                    if mx != -1:
+                        next_dp[j][c] = mx + val
+
+            dp, next_dp = next_dp, dp
+
+        return max(dp[n - 1])
