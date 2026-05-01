08d2d0a9724616e4e876589769150b6c210d420e
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-05-01 16:49:12 +0530

    Create solution.java

---
+++ 2026/05-May/01-Friday/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxRotateFunction(int[] nums) {
+        int n = nums.length;
+        long sum = 0, f = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            f += (long) i * nums[i];
+        }
+
+        long res = f;
+
+        for (int k = 1; k < n; k++) {
+            f = f + sum - (long) n * nums[n - k];
+            res = Math.max(res, f);
+        }
+
+        return (int) res;
+    }
+}
