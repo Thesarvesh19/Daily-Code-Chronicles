7e01ed6dc376458932b91bf3c16dd810644968b2
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-05-04 22:15:12 +0530

    Add Python solution for LeetCode 48 (Rotate Image) using transpose + reverse

--- 2026/05-May/04-Monday/solution.py
+++ 2026/05-May/04-Monday/solution.py
@@ -6,7 +6,7 @@ class Solution:
         for i in range(n):
             for j in range(i, n):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-        
+         
         # Step 2: Reverse each row
         for row in matrix:
             row.reverse()
