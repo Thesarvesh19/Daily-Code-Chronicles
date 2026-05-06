78725175298a1021199493e19928d4e3afa32fad
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-05-06 09:13:12 +0530

    Create solution.java

---
+++ 2026/05-May/06-Wednesday/solution.java
@@ -0,0 +1,39 @@
+class Solution {
+
+    public char[][] rotateTheBox(char[][] boxGrid) {
+
+        int m = boxGrid.length;
+        int n = boxGrid[0].length;
+
+        for (int i = 0; i < m; i++) {
+
+            int emptyPos = n - 1;
+
+            for (int j = n - 1; j >= 0; j--) {
+
+                if (boxGrid[i][j] == '*') {
+                    emptyPos = j - 1;
+                }
+
+                else if (boxGrid[i][j] == '#') {
+
+                    char temp = boxGrid[i][j];
+                    boxGrid[i][j] = boxGrid[i][emptyPos];
+                    boxGrid[i][emptyPos] = temp;
+
+                    emptyPos--;
+                }
+            }
+        }
+
+        char[][] ans = new char[n][m];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                ans[j][m - 1 - i] = boxGrid[i][j];
+            }
+        }
+
+        return ans;
+    }
+}
