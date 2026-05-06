4cbbbf1e14180a8d1d9a8e68f94aea987ecb0240
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-05-06 09:18:09 +0530

    Create solution.js

---
+++ 2026/05-May/06-Wednesday/solution.js
@@ -0,0 +1,35 @@
+var rotateTheBox = function(boxGrid) {
+
+    let m = boxGrid.length;
+    let n = boxGrid[0].length;
+
+    for (let i = 0; i < m; i++) {
+
+        let emptyPos = n - 1;
+
+        for (let j = n - 1; j >= 0; j--) {
+
+            if (boxGrid[i][j] === '*') {
+                emptyPos = j - 1;
+            }
+
+            else if (boxGrid[i][j] === '#') {
+
+                [boxGrid[i][j], boxGrid[i][emptyPos]] =
+                [boxGrid[i][emptyPos], boxGrid[i][j]];
+
+                emptyPos--;
+            }
+        }
+    }
+
+    let ans = Array.from({ length: n }, () => Array(m));
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            ans[j][m - 1 - i] = boxGrid[i][j];
+        }
+    }
+
+    return ans;
+};
