6f5cc574986e3783bac608e896f4148e6e6286d0
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-05-11 10:37:48 +0530

    add Java solution for LeetCode 2553

--- 2026/05-May/11-Monday/solution.java
+++ 2026/05-May/11-Monday/solution.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] separateDigits(int[] nums) {
         List<Integer> list = new ArrayList<>();
-
+ 
         for (int num : nums) {
             String s = String.valueOf(num);
 
