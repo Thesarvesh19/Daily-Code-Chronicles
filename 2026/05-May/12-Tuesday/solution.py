b4f5fd8e80f08a8388186fb90dde77625d6a8bf8
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-05-12 13:06:17 +0530

    Create solution.py

---
+++ 2026/05-May/12-Tuesday/solution.py
@@ -0,0 +1,17 @@
+# solution.py
+
+class Solution:
+    def minimumEffort(self, tasks):
+        tasks.sort(key=lambda x: (x[1] - x[0]), reverse=True)
+
+        total_energy_needed = 0
+        current_energy = 0
+
+        for actual, minimum in tasks:
+            if current_energy < minimum:
+                total_energy_needed += (minimum - current_energy)
+                current_energy = minimum
+
+            current_energy -= actual
+
+        return total_energy_needed
