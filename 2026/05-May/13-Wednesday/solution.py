0c8b6eb13c9257d351f53984bc330f3e84cf3ef9
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-05-13 23:41:56 +0530

    Create solution.py

---
+++ 2026/05-May/13-Wednesday/solution.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minMoves(self, nums: List[int], limit: int) -> int:
+        n = len(nums)
+        diff = [0] * (2 * limit + 2)
+
+        for i in range(n // 2):
+            a = nums[i]
+            b = nums[n - 1 - i]
+
+            low = min(a, b)
+            high = max(a, b)
+            total = a + b
+
+            diff[2] += 2
+            diff[low + 1] -= 1
+            diff[total] -= 1
+            diff[total + 1] += 1
+            diff[high + limit + 1] += 1
+
+        ans = float('inf')
+        curr = 0
+
+        for s in range(2, 2 * limit + 1):
+            curr += diff[s]
+            ans = min(ans, curr)
+
+        return ans
