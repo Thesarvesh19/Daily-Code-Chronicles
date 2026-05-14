402ec43a3a30bcbe4ec40eb0e7f9ceebfe6bd561
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-05-14 20:42:52 +0530

    Added C++ solution for LeetCode 2784 with array validation logic

--- 2026/05-May/14-Thursday/solution.cpp
+++ 2026/05-May/14-Thursday/solution.cpp
@@ -6,7 +6,7 @@ public:
         int n = nums.size();
 
         if(nums[n-1] != n-1 || nums[n-2] != n-1)
-            return false;
+            return false; 
 
         for(int i=0;i<n-2;i++){
             if(nums[i] != i+1)
