1487bb9295d3a25cf5c9ee654d994f20db913867
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-05-14 20:41:41 +0530

    Create solution.java

---
+++ 2026/05-May/14-Thursday/solution.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean isGood(int[] nums) {
+        Arrays.sort(nums);
+
+        int n = nums.length;
+
+        // Last two elements should be n-1
+        if (nums[n - 1] != n - 1 || nums[n - 2] != n - 1)
+            return false;
+
+        // First n-2 elements should be 1 to n-2
+        for (int i = 0; i < n - 2; i++) {
+            if (nums[i] != i + 1)
+                return false;
+        }
+
+        return true;
+    }
+}
