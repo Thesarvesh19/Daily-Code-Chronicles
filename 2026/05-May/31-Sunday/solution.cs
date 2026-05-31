95ff89dffd684ff45750364fb8381721c172ef55
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-05-31 13:02:32 +0530

    Add solution for asteroid destruction problem

---
+++ 2026/05-May/31-Sunday/solution.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public bool AsteroidsDestroyed(int mass, int[] asteroids) {
+        Array.Sort(asteroids);
+
+        long currentMass = mass;
+
+        foreach (int asteroid in asteroids) {
+            if (currentMass < asteroid) {
+                return false;
+            }
+            currentMass += asteroid;
+        }
+
+        return true;
+    }
+}
