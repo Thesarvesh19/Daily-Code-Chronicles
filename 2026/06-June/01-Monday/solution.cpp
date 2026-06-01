41bf60a74130429c7069062f04f69d6707e308c7
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-06-01 19:25:22 +0530

    Fix initialization of ans variable in minimumCost
    Initialize ans variable to 0 for calculating minimum cost.

--- 2026/06-June/01-Monday/solution.cpp
+++ 2026/06-June/01-Monday/solution.cpp
@@ -3,7 +3,7 @@ public:
     int minimumCost(vector<int>& cost) {
         sort(cost.begin(), cost.end(), greater<int>());
 
-        int ans = 0;
+        int ans = 0; 
 
         for (int i = 0; i < cost.size(); i++) {
             if (i % 3 != 2)
