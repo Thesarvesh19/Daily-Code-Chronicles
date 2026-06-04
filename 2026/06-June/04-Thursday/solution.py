68085a55246c4eb814781b1db4c50ecb183f7520
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-06-04 11:30:38 +0530

    Implement waviness and totalWaviness methods

---
+++ 2026/06-June/04-Thursday/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def waviness(self, num: int) -> int:
+        s = str(num)
+
+        if len(s) < 3:
+            return 0
+
+        count = 0
+
+        for i in range(1, len(s) - 1):
+            if ((s[i] > s[i - 1] and s[i] > s[i + 1]) or
+                (s[i] < s[i - 1] and s[i] < s[i + 1])):
+                count += 1
+
+        return count
+
+    def totalWaviness(self, num1: int, num2: int) -> int:
+        ans = 0
+
+        for num in range(num1, num2 + 1):
+            ans += self.waviness(num)
+
+        return ans
