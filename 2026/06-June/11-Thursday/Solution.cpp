aa4b58ba2f74ba8687786eae425c21144b9dedca
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-06-11 14:24:48 +0530

    Implement O(n) BFS solution for edge weight assignments in C++

--- 2026/06-June/11-Thursday/Solution.cpp
+++ 2026/06-June/11-Thursday/Solution.cpp
@@ -5,7 +5,7 @@ public:
     long long modPow(long long a, long long b) {
         long long res = 1;
         while (b) {
-            if (b & 1) res = res * a % MOD;
+            if (b & 1) res = res * a % MOD; 
             a = a * a % MOD;
             b >>= 1;
         }
