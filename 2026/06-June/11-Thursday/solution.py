137df0ecc3a4acb0ef46ff8ba7d08aac8084e44f
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-06-11 14:22:24 +0530

    Implement edge weight assignment in solution.py

---
+++ 2026/06-June/11-Thursday/solution.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+class Solution:
+    def assignEdgeWeights(self, edges):
+        MOD = 10**9 + 7
+
+        n = len(edges) + 1
+        g = [[] for _ in range(n + 1)]
+
+        for u, v in edges:
+            g[u].append(v)
+            g[v].append(u)
+
+        q = deque([1])
+        vis = {1}
+
+        depth = -1
+
+        while q:
+            depth += 1
+
+            for _ in range(len(q)):
+                u = q.popleft()
+
+                for v in g[u]:
+                    if v not in vis:
+                        vis.add(v)
+                        q.append(v)
+
+        return pow(2, depth - 1, MOD)
