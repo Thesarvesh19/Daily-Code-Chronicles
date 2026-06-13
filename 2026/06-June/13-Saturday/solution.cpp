ef1c9fe2786424c952852b0f846e27f7aa9d864f
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-06-13 22:47:04 +0530

    Create solution.cpp

---
+++ 2026/06-June/13-Saturday/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans;
+
+        for (const string& word : words) {
+            int sum = 0;
+
+            for (char c : word) {
+                sum += weights[c - 'a'];
+            }
+
+            sum %= 26;
+            ans.push_back('a' + (25 - sum));
+        }
+
+        return ans;
+    }
+};
