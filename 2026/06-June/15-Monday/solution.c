f34157095daa2ec7f442d40a40a20504382b17d8
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-06-15 12:52:46 +0530

    Create solution.c

---
+++ 2026/06-June/15-Monday/solution.c
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* deleteMiddle(struct ListNode* head) {
+    if (head == NULL || head->next == NULL) {
+        return NULL;
+    }
+
+    struct ListNode* slow = head;
+    struct ListNode* fast = head->next->next;
+
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    struct ListNode* temp = slow->next;
+    slow->next = slow->next->next;
+
+    return head;
+}
