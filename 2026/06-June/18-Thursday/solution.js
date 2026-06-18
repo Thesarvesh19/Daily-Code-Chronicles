6990d057baeb0b6b80f52d2456184c85aa6446cc
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-06-18 15:31:17 +0530

    Implement function to compute angle between clock hands

---
+++ 2026/06-June/18-Thursday/solution.js
@@ -0,0 +1,8 @@
+var angleClock = function(hour, minutes) {
+    const hourAngle = (hour % 12) * 30 + minutes * 0.5;
+    const minuteAngle = minutes * 6;
+
+    const diff = Math.abs(hourAngle - minuteAngle);
+
+    return Math.min(diff, 360 - diff);
+};
