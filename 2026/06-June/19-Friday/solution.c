3ce512450e17424363070a7c27955ccad317db29
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-06-19 11:56:10 +0530

    Create solution.c

---
+++ 2026/06-June/19-Friday/solution.c
@@ -0,0 +1,12 @@
+int largestAltitude(int* gain, int gainSize) {
+    int altitude = 0;
+    int highest = 0;
+
+    for (int i = 0; i < gainSize; i++) {
+        altitude += gain[i];
+        if (altitude > highest)
+            highest = altitude;
+    }
+
+    return highest;
+}
