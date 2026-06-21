2b4a21abcd5795eaa5d002f9ed126a47da640e86
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-06-21 13:53:17 +0530

    Implement MaxIceCream function to count ice creams

---
+++ 2026/06-June/21-Sunday/solution.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Solution {
+    public int MaxIceCream(int[] costs, int coins) {
+        Array.Sort(costs);
+
+        int count = 0;
+
+        foreach (int cost in costs) {
+            if (coins < cost)
+                break;
+
+            coins -= cost;
+            count++;
+        }
+
+        return count;
+    }
+}
