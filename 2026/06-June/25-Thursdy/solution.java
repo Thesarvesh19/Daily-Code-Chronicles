7b80901c999ffbfe2f89cba35c1e277bf22b9058
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-06-25 23:12:21 +0530

    Add solution for counting majority subarrays

---
+++ 2026/06-June/25-Thursdy/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countMajoritySubarrays(int[] nums, int target) {
+        int n = nums.length;
+        int ans = 0;
+        
+        for (int i = 0; i < n; ++i) {
+            int cnt = 0;
+            for (int j = i; j < n; ++j) {
+                if (nums[j] == target) {
+                    cnt++;
+                }
+                int length = j - i + 1;
+                if (cnt * 2 > length) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}
