1ff0512a9a6b5312e49232423726d5196dcac4c2
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-06-28 23:50:11 +0530

    Add solution for maximum element problem
    Implement function to find maximum element after decrementing and rearranging an array.

---
+++ 2026/06-June/28-Sunday/solution.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int maximumElementAfterDecrementingAndRearranging(int* arr, int arrSize) {
+    qsort(arr, arrSize, sizeof(int), compare);
+    arr[0] = 1;
+    for (int i = 1; i < arrSize; i++) {
+        if (arr[i] > arr[i - 1] + 1) {
+            arr[i] = arr[i - 1] + 1;
+        }
+    }
+    return arr[arrSize - 1];
+}
