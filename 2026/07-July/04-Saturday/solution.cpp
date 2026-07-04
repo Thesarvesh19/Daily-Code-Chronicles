52b3008e599527f59b4c64c139e79eb72c842497
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-07-04 22:58:40 +0530

    Implement minScore function to find minimum road score

---
+++ 2026/07-July/04-Saturday/solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minScore(int n, vector<vector<int>>& roads) {
+        vector<vector<pair<int, int>>> adj(n + 1);
+
+        for (auto &road : roads) {
+            int u = road[0], v = road[1], d = road[2];
+            adj[u].push_back({v, d});
+            adj[v].push_back({u, d});
+        }
+
+        vector<bool> vis(n + 1, false);
+        queue<int> q;
+        q.push(1);
+        vis[1] = true;
+
+        int ans = INT_MAX;
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            for (auto &[next, dist] : adj[node]) {
+                ans = min(ans, dist);
+                if (!vis[next]) {
+                    vis[next] = true;
+                    q.push(next);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
