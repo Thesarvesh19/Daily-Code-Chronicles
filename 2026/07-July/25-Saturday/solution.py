704eded5c15f8953a7bd21758600c920ff6ab700
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-07-25 21:20:18 +0530

    Add solution.py to find max product of digits

---
+++ 2026/07-July/25-Saturday/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProduct(self, n: int) -> int:
+        largest = 0
+        second = 0
+
+        while n:
+            digit = n % 10
+            n //= 10
+
+            if digit >= largest:
+                second = largest
+                largest = digit
+            elif digit > second:
+                second = digit
+
+        return largest * second
