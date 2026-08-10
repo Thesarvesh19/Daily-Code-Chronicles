a2aeb93a28011ab75d944cfebd26c083ebf9de02
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-08-10 23:29:19 +0530

    Add solution for winnerSquareGame problem

---
+++ 2026/10-Monday/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean winnerSquareGame(int n) {
+        boolean[] dp = new boolean[n + 1];
+
+        // dp[i] = true if the current player can win with i stones
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                // If removing j*j leaves a losing position,
+                // current player can win.
+                if (!dp[i - j * j]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
