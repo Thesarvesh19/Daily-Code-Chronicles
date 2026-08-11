e853bb78652fbe1991e975e36d919d6c51ca4bde
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-08-11 23:43:54 +0530

    Implement method to find missing integer in array

---
+++ 2026/11-Tuesday/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int missingInteger(int[] nums) {
+        int sum = nums[0];
+
+        // Sum of the longest sequential prefix
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1] + 1) {
+                sum += nums[i];
+            } else {
+                break;
+            }
+        }
+
+        // Store all elements
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        // Find smallest missing integer >= sum
+        while (set.contains(sum)) {
+            sum++;
+        }
+
+        return sum;
+    }
+}
