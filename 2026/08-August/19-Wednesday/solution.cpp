97be21062616de72a533fb752b14110f81d0d49b
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-08-19 22:59:00 +0530

    Implement maxNumberOfFamilies function in solution.cpp

---
+++ 2026/08-August/19-Wednesday/solution.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
+        unordered_map<int, unordered_set<int>> reserved;
+
+        for (auto &seat : reservedSeats) {
+            reserved[seat[0]].insert(seat[1]);
+        }
+
+        int ans = (n - reserved.size()) * 2;
+
+        for (auto &[row, seats] : reserved) {
+            bool left = !seats.count(2) && !seats.count(3) &&
+                        !seats.count(4) && !seats.count(5);
+
+            bool middle = !seats.count(4) && !seats.count(5) &&
+                          !seats.count(6) && !seats.count(7);
+
+            bool right = !seats.count(6) && !seats.count(7) &&
+                         !seats.count(8) && !seats.count(9);
+
+            if (left && right)
+                ans += 2;
+            else if (left || middle || right)
+                ans += 1;
+        }
+
+        return ans;
+    }
+};
