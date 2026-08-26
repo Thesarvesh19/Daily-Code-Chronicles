c95a813f9074ce076299e600ab4960a8c5d4115c
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-08-26 23:22:34 +0530

    Update solution.py

--- 2026/08-August/26-Wednesday/solution.py
+++ 2026/08-August/26-Wednesday/solution.py
@@ -1,1 +1,30 @@
-g
+class Solution:
+    def shortestBeautifulSubstring(self, s: str, k: int) -> str:
+        left = 0
+        ones = 0
+        ans = ""
+
+        for right in range(len(s)):
+            if s[right] == '1':
+                ones += 1
+
+            # Too many ones -> move left
+            while ones > k:
+                if s[left] == '1':
+                    ones -= 1
+                left += 1
+
+            # Remove unnecessary leading zeros
+            while ones == k and s[left] == '0':
+                left += 1
+
+            # Valid beautiful substring
+            if ones == k:
+                curr = s[left:right + 1]
+
+                if (not ans or
+                    len(curr) < len(ans) or
+                    (len(curr) == len(ans) and curr < ans)):
+                    ans = curr
+
+        return ans
