983a980e34046c6a8429be9dc1f87480167fe032
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-09-06 23:04:42 +0530

    Create solution.cpp

---
+++ 2026/09-September/06-Sunday/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n = t.size();
+        vector<unsigned long long> dp(n + 1, 0);
+        dp[0] = 1;
+
+        for (char ch : s) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (ch == t[j]) {
+                    dp[j + 1] += dp[j];
+                }
+            }
+        }
+
+        return (int)dp[n];
+    }
+};
