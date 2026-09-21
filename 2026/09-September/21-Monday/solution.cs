91561b07c431ae9d1deb52a363d582527cb6c8ae
Sarvesh Soumil <the19sarvesh@gmail.com>
2026-09-21 08:57:04 +0530

    Add ResultArray method for subarray calculations
    Implement a method to calculate the number of subarrays with a specific modulo result.

---
+++ 2026/09-September/21-Monday/solution.cs
@@ -0,0 +1,37 @@
+public class Solution
+{
+    public int[] ResultArray(int[] nums, int k)
+    {
+        int[] ans = new int[k];
+        int[] dp = new int[k];
+
+        foreach (int num in nums)
+        {
+            int mod = num % k;
+            int[] newDp = new int[k];
+
+            // Start a new subarray with only num
+            newDp[mod]++;
+
+            // Extend previous subarrays
+            for (int r = 0; r < k; r++)
+            {
+                if (dp[r] > 0)
+                {
+                    int newRemainder = (r * mod) % k;
+                    newDp[newRemainder] += dp[r];
+                }
+            }
+
+            // Add subarrays ending at current position
+            for (int r = 0; r < k; r++)
+            {
+                ans[r] += newDp[r];
+            }
+
+            dp = newDp;
+        }
+
+        return ans;
+    }
+}
